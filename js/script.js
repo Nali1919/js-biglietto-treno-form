ee46166ac461bf07ae908ad63480d624ce1e42c6
Nali1919 <my@email.com>
2022-08-24 17:04:11 +0200

    Creo struttura e input

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('js.ok')
+const addKm = document.getElementById('addKm')
+const age = document.getElementById('age')
+const bottone = document.getElementById('bottone')
+
+
+bottone.addEventListener('click', function () {
+    let addAge = parseInt(age.value)
+    console.log(addAge)
+
+
+   
+})
